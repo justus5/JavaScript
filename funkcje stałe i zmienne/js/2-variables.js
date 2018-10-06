fa22ca5c4950a847796d5fe2f3055789ae22c5f3
justwiesek <jwiesek@gmail.com>
2018-10-06 10:13:37 +0200

    zmienne i stałe

---
+++ funkcje stałe i zmienne/js/2-variables.js
@@ -0,0 +1,18 @@
+console.log("Działa variables");
+
+let imie; 
+let imieMeskie = "Kacper";
+
+imie = "Monika";
+imie = "Gosia";
+imie = imieMeskie;
+
+// jesli "X" mamy nazwę, jeśli tylko X w postaci liczby to wtedy mamy liczbę
+
+let liczba = "5000";
+
+//jesli mamy const wtedy tej wartosci nie możemy zmienić 
+const innaLiczba = 5000;
+innaLiczba = 1000;
+
+console.log(imie);
---
+++ funkcje stałe i zmienne/js/js-comments.js
@@ -0,0 +1,8 @@
+//console.log("Działa!"); - komentarz liniowy
+
+
+/*
+console.log("działa!");
+console.log("działa!");
+*/
+
