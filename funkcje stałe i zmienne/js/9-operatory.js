93df27f061a796e4259c87da7cf64d9b815686b8
justwiesek <jwiesek@gmail.com>
2018-10-06 16:17:47 +0200

    operatory js

---
+++ funkcje stałe i zmienne/js/9-operatory.js
@@ -0,0 +1,17 @@
+let wartosc = (2 + 2 > 4 - 1);
+
+// "!" jest zaprzeczeniem, zmienia sie wynik operacji na przeciwną
+wartosc = !(10 + 10 > 100);
+
+
+
+wartosc = (2 + 2 === 4 && 3 + 1 === 4);
+
+wartosc = (2 + 2 === 12 || 3 + 1 === 4);
+
+
+//ternary - warunki
+
+wartosc = (2 + 2 === 4) ? "rowna się 4" : "nie rowna się 4";
+
+console.log(wartosc);
