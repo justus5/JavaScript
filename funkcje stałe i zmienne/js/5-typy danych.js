64d3fe2a8922a6106fdc448d4efe36e17b5549f2
justwiesek <jwiesek@gmail.com>
2018-10-06 12:56:34 +0200

    typy danych

---
+++ funkcje stałe i zmienne/js/5-typy danych.js
@@ -0,0 +1,20 @@
+let liczba = 50;
+let tekst = "Tu bedzie tekst - string";
+let trueFalse = true;
+let tablica = [1,2,3].length; // tablica[0] --> 1
+let object = {
+    klucz: "wartość"};  //aby dostac sie do elementu wpisujemy object["klucz"] lub object.klucz
+
+console.log(object.klucz);
+
+// ES5
+
+let number = 10;
+let zdanie = "Wyświetl " + number + " i zakończ działanie";
+
+// ES6 
+
+let number2 = 10;
+let zdanie2 = `Wyświetl ${number2} + i zakończ działanie`;
+
+console.log(zdanie);
