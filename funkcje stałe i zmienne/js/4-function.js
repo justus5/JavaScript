0143484bbc55d78924f504f96c2a86f95086643a
justwiesek <jwiesek@gmail.com>
2018-10-06 12:56:17 +0200

    cd function

---
+++ funkcje stałe i zmienne/js/4-function.js
@@ -0,0 +1,29 @@
+//let number = 5; 
+//console.log(number);
+//
+//function showNumber(num) {
+//    let number = "w srodku " + 15;
+//    
+//    console.log(number);
+//    // return konczy działanie, nie może byc przed console, bo wtedy wykaże błąd i nie wyswietli się ["w srodku " + 15]
+//    return num;
+//}
+//
+//console.log(showNumber(10));
+
+let number = 5; 
+console.log(number);
+
+function showNumber(num) {
+    let number = "w srodku " + 15;
+    console.log(number);
+    
+    if (2 + 2 == 4) {
+    let number = "w srodku if () " + 50;
+    console.log(number);
+}
+return num;
+}
+
+showNumber(10);
+
