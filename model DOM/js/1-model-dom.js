0619a4e25cc744db65d871d5d4f8d064953e11c1
justwiesek <jwiesek@gmail.com>
2018-10-07 10:42:13 +0200

    model DOM js

---
+++ model DOM/js/1-model-dom.js
@@ -0,0 +1,10 @@
+// document jest obiektem modelu DOM i występuje zawsze 
+
+//console.log(document);
+//console.log(window);
+
+// traversing
+
+/*console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);*/
